a7c1abbddb4314d69c25a1f34ec092daceefd648
= <eilersd23@gmail.com>
2023-08-30 14:49:43 -0400

    created database of languages and corresponding information

---
+++ languages.py
@@ -0,0 +1,6 @@
+import emoji
+
+language_dat = {"EN-US": {"Language": "English", "Flags": ["United_Kingdom"], "Icon": "🇬🇧"},
+                "ES": {"Language": "Spanish", "Flags": ["Spain"], "Icon": "🇪🇸"},
+                "DE": {"Language": "German", "Flags": ["Germany"], "Icon": "🇩🇪"},
+                "FR": {"Language": "French", "Flags": ["France"], "Icon": "🇫🇷"}}

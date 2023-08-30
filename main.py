fc17001897825fbe150e1ba0b4aa1b68928c633b
= <eilersd23@gmail.com>
2023-08-29 20:44:14 -0400

    added basic functionality for adding languages via reactions

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from dotenv import load_dotenv
 
 import discord
 from discord.ext import commands
+import asyncio
+import emoji
 
-from translate import translate_message
+# from translate import translate_message
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -15,16 +17,36 @@ intents.typing = True
 intents.presences = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("$"),intents=intents)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("--"),intents=intents)
+
+languages = []
 
 @bot.event
 async def on_message(message):
     if bot.user.mentioned_in(message):
         await message.channel.send('Hello there!')
     await bot.process_commands(message)
+    
 
-@bot.command()
-async def foo(ctx):
-    await ctx.send("Hello!")
+@bot.command("add")
+async def add_language(ctx):
+    channel = ctx.channel
+    # react = ["📰"]
+    
+    mes = await channel.send('Which language would you like to add?')
+    # for tmp in react:
+    #     await mes.add_reaction(tmp)
+    
+    def check(reaction, user):
+        return user == ctx.author
+    
+    try:
+        reaction, user = await bot.wait_for('reaction_add', timeout=10.0, check=check)
+    except asyncio.TimeoutError:
+        await channel.send('👎')
+    else:
+        language = emoji.demojize(str(reaction) + " ")[1:-2]
+        languages.append(language)
+        await ctx.send("Added " + language)
 
 bot.run(TOKEN)

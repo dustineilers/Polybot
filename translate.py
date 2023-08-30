282bb55599eaf2150bbebb30f276dc75ac90a6aa
= <eilersd23@gmail.com>
2023-08-30 14:50:16 -0400

    created module utilizing deepl for message translation

---
+++ translate.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+import deepl
+
+load_dotenv()
+auth_key = os.getenv('AUTH_KEY')
+translator = deepl.Translator(auth_key)
+
+def translate_message(text, target_lang = "EN-US"):
+
+    translation = translator.translate_text(text, target_lang=target_lang)
+
+    translated_message = str(translation)
+    source_language = translation.detected_source_lang
+
+    return translated_message, source_language

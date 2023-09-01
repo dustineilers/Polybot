0cdfcac453977040f354fbb1de47dc7aa9857646
= <eilersd23@gmail.com>
2023-08-31 20:48:31 -0400

    restructed repository

--- polybot/setup.py
+++
@@ -1,9 +0,0 @@
-import setup from setuptools
-
-setup(
-    name='polybot',
-    version='1.0',
-    description='a translation discord bot',
-    author='dustin eilers',
-    author_email='eilersd23@gmail.com',
-)

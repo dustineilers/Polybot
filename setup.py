facf777bc0389d298ff8b123f03c4777ed6b77f8
= <eilersd23@gmail.com>
2023-08-31 20:52:59 -0400

    included requirements into setup file:

--- setup.py
+++ setup.py
@@ -6,4 +6,13 @@ setup(
     description='a translation discord bot',
     author='dustin eilers',
     author_email='eilersd23@gmail.com',
+    install_requires=[
+        'async-timeout==4.0.3',
+        'colorama==0.4.6',
+        'deepl==1.15.0',
+        'discord==2.3.2',
+        'discord.py==2.3.2',
+        'emoji==2.8.0',
+        'python-dotenv==1.0.0'
+    ]
 )
